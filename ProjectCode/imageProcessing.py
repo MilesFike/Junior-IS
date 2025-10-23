faeb2d2db1d40c662f7fe586d1b8ec867b746614
Miles Fike <mfike27@wooster.edu>
2025-10-23 09:42:39 -0400

    Added imageProcessing.py file to perform image processing for preparation for image input to computer vision system: Still incomplete needs to be reduced to 28 pixels and centered

---
+++ ProjectCode/imageProcessing.py
@@ -0,0 +1,48 @@
+from PIL import Image
+from PIL import ImageOps#Helps create original black but textured background with color import
+import cv2 #cv2 used for more background conversions
+import numpy 
+import time
+
+
+if __name__ == "__main__":
+
+    imPathM = "imgs\m.jpg"
+    
+
+    m2 = cv2.imread(imPathM)
+    #converts to gray scale
+    gray = cv2.cvtColor(m2, cv2.COLOR_BGR2GRAY)
+
+    #Pixels above threshold become white. These are then replaced with black
+    _, mask = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
+    black_background = numpy.zeros_like(m2)
+
+    result = cv2.bitwise_and(m2, m2, mask=mask) + black_background
+    cv2.imshow("m2", m2)
+    #checks for all black pixels
+
+    cv2.imwrite("imgs\m2.png", result)
+
+
+    img = Image.open("imgs\m2.png")
+    #collecting data for iteration
+    pixels = img.load()
+    width, height, = img.size
+    for x in range(width):
+        for y in range(height):
+            currentColor = pixels[x,y]
+            #print(currentColor) Do not uncomment this line
+            if currentColor != (0, 0, 0):
+                pixels[x,y] = (255, 255, 255)
+    img.show()
+    time.sleep(10)
+    img_resized = img.resize((128, 128))
+    img.show()
+    img.save("imgs\m2.png")
+    #Transfering back to cv2 for Gaussian Blur
+    m2 = cv2.imread("imgs\m2.png")
+    m2 = cv2.GaussianBlur(m2, (5, 5), 1)
+    cv2.imwrite("imgs\m2.png", m2)
+
+
